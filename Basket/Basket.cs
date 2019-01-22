bfd7fb10a2ae5c5df63041a47d419b4bb375ed73
Jan Imrichovsky <jan.imrichovsky@interoute.com>
2019-01-22 22:22:54 +0000

    Test passed

--- Basket/Basket.cs
+++ Basket/Basket.cs
@@ -17,7 +17,15 @@ namespace Basket
 
         public decimal GetTotal()
         {
-            return _items.Sum(t => t.Price);
+            var total = _items.Sum(t => t.Price);
+            if (_items.Count(t => t.Name == StaticProductCatalog.Butter.Name) > 1)
+            {
+                if (_items.Any(t => t.Name == StaticProductCatalog.Bread.Name))
+                {
+                    total -= StaticProductCatalog.Bread.Price * 0.5m;
+                }
+            }
+            return total;
         }
     }
 }
