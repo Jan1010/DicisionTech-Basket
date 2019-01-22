999e2bcbf672e09bb1ccc7c7ae3efbcded91bed9
Jan Imrichovsky <jan.imrichovsky@interoute.com>
2019-01-22 21:27:28 +0000

    Implemented logic to pass add product test

---
+++ Basket/Basket.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Basket
+{
+    public class Basket
+    {
+        public List<Product> Items { get; set; } = new List<Product>();
+        public void AddProduct(Product product)
+        {
+            Items.Add(product);
+        }
+    }
+}
