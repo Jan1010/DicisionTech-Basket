f77518615b50632ffb4c2e516b0a40ab2ab40b3e
Jan Imrichovsky <jan.imrichovsky@interoute.com>
2019-01-22 22:58:31 +0000

    Added test for TwoButtersHalfPriceBreadOffer

---
+++ Basket.Test/Offer/TwoButtersHalfPriceBreadOfferTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Basket.Offer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Basket.Test.Offer
+{
+    [TestClass]
+    public class TwoButtersHalfPriceBreadOfferTests
+    {
+        [TestMethod]
+        public void Given2ButtersAnd1Bread_WhenDiscountRequested_ThenDiscountShouldBeHalfOfBread()
+        {
+            //Arrange
+            TwoButtersHalfPriceBreadOffer offer = new TwoButtersHalfPriceBreadOffer();
+            List<Product> products = new List<Product>
+            {
+                StaticProductCatalog.Butter,
+                StaticProductCatalog.Butter,
+                StaticProductCatalog.Bread,
+            };
+
+            //Act
+            var discount = offer.GetDiscount(products);
+
+            //Assert
+            Assert.AreEqual(StaticProductCatalog.Bread.Price * 0.5m, discount);
+        }
+    }
+}
