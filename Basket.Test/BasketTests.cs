3425e4ced21328b3ee4be7a97ff14182e5d8e7cc
Jan Imrichovsky <jan.imrichovsky@interoute.com>
2019-01-22 21:47:17 +0000

    More test on AddProduct method added. All succesfull

--- Basket.Test/BasketTests.cs
+++ Basket.Test/BasketTests.cs
@@ -20,5 +20,51 @@ namespace Basket.Test
             Assert.AreEqual(1, basket.Items.Count);
             Assert.IsTrue(basket.Items.Contains(butter));
         }
+
+        [TestMethod]
+        public void GivenTwoProducts_WhenAddedToBasket_ThenBasketContainsThem()
+        {
+            //Arrange
+            var basket = new Basket();
+            var butter = new Product { Name = "butter", Price = 0.8m };
+            var milk = new Product { Name = "milk", Price = 1.15m };
+
+            //Act
+            basket.AddProduct(butter);
+            basket.AddProduct(milk);
+
+            //Assert
+            Assert.AreEqual(2, basket.Items.Count);
+            Assert.IsTrue(basket.Items.Contains(butter));
+            Assert.IsTrue(basket.Items.Contains(milk));
+        }
+
+        [TestMethod]
+        public void GivenEmptyBasket_WhenNothingIsAdded_ThenBasketHasZeroItems()
+        {
+            //Arrange
+            var basket = new Basket();
+
+            //Act
+
+            //Assert
+            Assert.AreEqual(0, basket.Items.Count);
+        }
+
+        [TestMethod]
+        public void GivenBreadProduct_WhenAddedTwiceToBasket_BasketShoudHaveTwoBreads()
+        {
+            //Arrange
+            var basket = new Basket();
+            var bread = new Product { Name = "bread", Price = 1.00m };
+
+            //Act
+            basket.AddProduct(bread);
+            basket.AddProduct(bread);
+
+            //Assert
+            Assert.AreEqual(2, basket.Items.Count);
+            Assert.IsTrue(basket.Items.All(t => t == bread));
+        }
     }
 }
