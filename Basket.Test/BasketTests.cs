fba30e64cb12f57c1cc7facb1a1fc03318e604eb
Jan Imrichovsky <jan.imrichovsky@interoute.com>
2019-01-22 21:19:17 +0000

    Add product to basket test

---
+++ Basket.Test/BasketTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Basket.Test
+{
+    [TestClass]
+    public class BasketTests
+    {
+        [TestMethod]
+        public void GivenButterProduct_WhenAddedToBasket_ThenItShouldBeInBasket()
+        {
+            //Arrange
+            var basket = new Basket();
+            var butter = new Product { Name = "butter", Price = 0.8m };
+
+            //Act
+            basket.AddProduct(butter);
+
+            //Assert
+            Assert.AreEqual(1, basket.Items.Count);
+            Assert.IsTrue(basket.Items.Contains(butter));
+        }
+    }
+}
